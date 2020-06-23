ce2937957014a6a400e0a469a420211801924370
ChayaGreisman <chayamgreisman@gmail.com>
2020-06-23 14:43:29 -0700

    finished..

--- src/index.js
+++ src/index.js
@@ -14,9 +14,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const button = document.querySelector("form")[1]
   button.addEventListener("click", function(e) {
+    e.preventDefault();
     const inputText = document.getElementById("new-task-description").value
     const taskLi = createLi(inputText)
     renderLi(taskLi)
-    e.preventDefault();
   });
 });
