ebe1b672d50bce6d8323845be6cc8c4081cb068d
Ideal-Poster <malcolmgourdine@protonmail.com>
2020-06-23 16:55:20 -0400

    changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let inputValue = "";
+
+  // keypress listener 
+  let input = document.getElementById("new-task-description");
+  input.addEventListener("keyup", function(event) {
+    inputValue = event.target.value;
+  });
+
+
+  // submit listener
+  let button = document.getElementById("submit-button");
+  button.addEventListener("click", function(event) {
+    event.preventDefault();
+    var idCounter = 0;
+    let newTaskElement = document.createElement("li");
+        newTaskElement.innerHTML = inputValue;
+        newTaskElement.id = `task-${idCounter}`;
+    let tasksElement = document.getElementById("tasks");    
+    tasksElement.append(newTaskElement);
+    idCounter += 1;
+  });
+
+  let listItem = document.getElementById("")
+
 });
