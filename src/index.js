3b49df32890e42b835b2244cd879bfbf149f0eb5
emotionalproof <chaserabenn@gmail.com>
2020-06-23 17:59:40 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+  // variable for text field
+  const taskDescription = document.querySelector("input#new-task-description")
+  // variable for form
+ 
+  // task list
+  // 
+  function createTaskLi(input){
+    const taskLi = document.createElement('li');
+    taskLi.className = "task";
+    taskLi.innerHTML = input;
+    return taskLi;
+  }
+
+  function addTaskLi(taskLi){
+    const taskUl = document.querySelector("ul#tasks");
+    taskUl.append(taskLi)
+  }
+  let form = document.querySelector("form")
+  form.addEventListener('submit', function(e){
+    e.preventDefault();
+    const taskDescription = document.querySelector("input#new-task-description")
+    const input = taskDescription.value;
+    if (input !== '') {
+      const li = createTaskLi(input)
+      addTaskLi(li);
+      form = document.querySelector("form").reset();
+    }
+    
+  })
+})
+
