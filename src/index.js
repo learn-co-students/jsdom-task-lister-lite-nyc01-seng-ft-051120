42b2e5a6689ca1ca77717b64c38ae1b0cd804005
JeanmarieJackman <jeanmarie.jackman@gmail.com>
2020-06-23 16:12:48 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let button = document.getElementById('button');
+  button.addEventListener('click', function(e){
+    e.preventDefault()
+    let task = document.getElementById('new-task-description').value
+    const ul = document.getElementById("tasks");
+    const li = document.createElement("li")
+    li.appendChild(document.createTextNode(task));
+    ul.appendChild(li);
+    let form = document.getElementById('create-task-form').reset();
+    })
+  
 });
+
+
+
