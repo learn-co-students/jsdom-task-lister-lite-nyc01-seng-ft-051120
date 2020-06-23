772c157b5e149702d3cae52fa1384803138be6ca
callas4ever <thetseringnorbu@gmail.com>
2020-06-23 17:07:13 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+ const task = document.getElementById("create-task-form");
+  task.addEventListener("submit", function(event) { 
+    event.preventDefault();
+    // console.log('welcome!')
+    const description = document.getElementById("new-task-description");
+    let li = document.createElement("li");
+    li.innerHTML = description.value;
+    document.getElementById("tasks").appendChild(li);
 });
+});
+
