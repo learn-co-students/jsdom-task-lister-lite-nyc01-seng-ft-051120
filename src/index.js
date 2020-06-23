6d0796b3a381e8f58d2c2e9e6fdb12b16cd6177d
aromsa <annacbateman@gmail.com>
2020-06-23 19:06:37 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.getElementById("create-task-form")
+  taskForm.addEventListener("submit", function(event){
+  // callback
+    event.preventDefault();
+    const task = taskForm.getElementsByTagName('input')[0].value
+    // taskForm.getElementsByTagName('input')[0].value = ''
+    const list = document.getElementById('tasks')
+    const item = document.createElement('li')
+    item.innerText = task
+    list.appendChild(item)
+    // console.log(task)
+    event.target.reset()
+  })
 });
+
+
+
+
