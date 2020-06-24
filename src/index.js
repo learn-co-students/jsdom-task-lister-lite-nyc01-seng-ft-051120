9c5e6c02303d814fadc2f72e109f17f5b2285393
chloekim917 <chloekim917@gmail.com>
2020-06-24 17:59:23 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  function createLi(userInput) {
+    let newTask = document.createElement("li")
+    newTask.className = "task"
+    newTask.innerHTML = userInput
+    return newTask
+  }
+
+  function renderLi(taskLi) {
+    const inputTasks = document.getElementById("tasks")
+    inputTasks.append(taskLi)
+  }
+
+  const addTasks = document.querySelector("#create-task-form")[1]
+  addTasks.addEventListener("click", function(e) {
+    const inputText = document.getElementById("new-task-description").value
+    const taskLi = createLi(inputText)
+    renderLi(taskLi)
+    e.preventDefault();
+  });
 });
+
