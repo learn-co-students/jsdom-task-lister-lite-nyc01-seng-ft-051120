70dbfd379070eecc85682fd36e09e644c2276c9b
smatalha <sma.talha@gmail.com>
2020-06-23 20:08:16 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const newTaskForm = document.getElementById("create-task-form");
   const newTaskDescription = document.getElementById("new-task-description");
-  const newTaskPriority = document.getElementById("new-task-priority");
 
   const newTaskUl = document.getElementById("tasks");
 
