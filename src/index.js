8ae530bd6ea4987f9f7909f0910c2c3074c00b9c
Ideal-Poster <malcolmgourdine@protonmail.com>
2020-06-23 23:35:32 -0400

    do over

--- src/index.js
+++ src/index.js
@@ -1,27 +1,42 @@
+let id = 0;
+class Task {
+  constructor(text) {
+    this.element = document.createElement("li");
+    this.element.innerText = text;
+    this.element.id = `task-${id}`;
+    id += 1;
+  }
+}
+class TaskList {
+  constructor() {
+    this.tasks = [];
+  }
+  
+  addTask(task) {
+    this.tasks.push(task);
+  }
+
+  render() {
+    let taskListElement = document.getElementById("tasks");
+    this.tasks.forEach(function(task) {
+      taskListElement.append(task.element);
+    });
+  }
+}
+
+let taskList = new TaskList();
+let task = new Task("hello rould");
+taskList.addTask(task)
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-  let inputValue = "";
+  let taskList = new TaskList();
+  let submitButton = document.getElementById("submit-button");
 
-  // keypress listener 
-  let input = document.getElementById("new-task-description");
-  input.addEventListener("keyup", function(event) {
-    inputValue = event.target.value;
-  });
-
-
-  // submit listener
-  let button = document.getElementById("submit-button");
-  button.addEventListener("click", function(event) {
+  submitButton.addEventListener("click", function(event) {
     event.preventDefault();
-    var idCounter = 0;
-    let newTaskElement = document.createElement("li");
-        newTaskElement.innerHTML = inputValue;
-        newTaskElement.id = `task-${idCounter}`;
-    let tasksElement = document.getElementById("tasks");    
-    tasksElement.append(newTaskElement);
-    idCounter += 1;
+    let inputElement = document.getElementById("new-task-description");
+    let text = inputElement.value;
+    taskList.addTask(new Task(text));
+    taskList.render();
   });
-
-  let listItem = document.getElementById("")
-
 });
