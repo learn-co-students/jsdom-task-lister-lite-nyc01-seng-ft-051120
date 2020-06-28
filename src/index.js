fcda6bc2d2946cb06072f62e08894ca1ecae0d9b
JulianCedric <julian.pormentilla@gmail.com>
2020-06-28 18:19:11 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  const button = document.querySelector('form')[1]
+
+  button.addEventListener("submit", function(event) {
+      event.preventDefault();
+      // ^ensures callback action is executed w/o refreshing page 
+
+      const userInput = document.getElementById('new-task-description').value;
+
+      const tasks = document.getElementById('tasks')
+
+      const newTask = document.createElement('li')
+      newTask.innerText = userInput 
+      tasks.appendChild(newTask)  
+
+      event.target.reset() 
+
+      });
 });
+
+
+//          
+
+
+  // function createLi(text) {
+  //   let newTask = document.createElement('li')
+  //   newTask.className = 'task'
+  //   newTask.innerHTML = text 
+  //   return newTask
+  //   }
+
+  // function appendUl(x) {
+  //   tasks.append(x) 
+  // }
